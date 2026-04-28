ea14355f8ca0089d8a215678cf46329ea7cff3a3
mokev10 <mokev10@outlook.com>
2026-04-28 16:13:26 +0200

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+st.title("🏠 Accueil")
+st.write("Cette application permet de compresser et décompresser des fichiers avec l’algorithme Zstandard.")
+st.info("Utilisez le menu latéral pour accéder aux fonctionnalités.")
