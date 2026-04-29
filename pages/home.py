9df3a4b423d5214cb2da052aeaf0280750c7d2ee
mokev10 <mokev10@outlook.com>
2026-04-29 11:42:54 +0200

    Delete pages directory

--- pages/home.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("🏠 Accueil")
-st.write("Cette application permet de compresser et décompresser des fichiers avec l’algorithme Zstandard.")
-st.info("Utilisez le menu latéral pour accéder aux fonctionnalités.")
